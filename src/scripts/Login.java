e589bf5e61fe667497dfd9cf6e3565788159bce7
Vijay Goli <vgoli@paypal.com>
2016-02-27 19:10:42 +0530

    adding a new line via fork

--- src/scripts/Login.java
+++ src/scripts/Login.java
@@ -5,6 +5,7 @@ public class Login {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Adding new line");
+System.out.println("Adding new line VIA FORK");
 	}
 
 }
